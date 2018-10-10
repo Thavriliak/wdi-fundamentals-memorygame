8a3b0ccfef59bed8ce8e763e5252f94407dab6e3
Thavriliak <Havriliakt@gmail.com>
2018-10-10 11:42:21 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards [2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log(cardsInPlay);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+// console.log("User flipped " + cardFour);
+console.log("user flipped " + cardsInPlay[1]);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert = "you found a match!"
+}
+else {
+	alert = "Sorry, try again."
+}
+console.log(alert)
+
